d045111f5d2050af9097d515e70115e431fb792b
mdslima <155018160+mdslima@users.noreply.github.com>
2023-12-29 15:36:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import streamlit as st 
 
+# Colocar o nome do trabalho em negrito
+st.markdown("<b>Trabalho final</b>", unsafe_allow_html=True)
+
+
+# Colocar o nome do aluno em negrito
+st.markdown("Alunos: <b>Marcelo da Silva Lima e Jucimar Cerqueira dos Santos</b>", unsafe_allow_html=True)
+
 
 st.header(' App da Equipe')
